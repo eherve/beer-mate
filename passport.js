1bf58d58ba0b5f8c83287b054f945636a62126eb
Eric Herve <eric.herve.95@gmail.com>
2015-01-24 10:53:08 +0100

    Fix wrong user nodel in passport

--- passport.js
+++ passport.js
@@ -2,11 +2,12 @@
 
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-var UserModel = require('mongoose').model('User');
+var UserModel = require('./models/user');
 
 passport.use(new LocalStrategy(
   { usernameField: 'email' },
   function(email, password, done) {
+  console.log('auth user:', email, password);
     UserModel.authenticate(email, password, function (err, user) {
       if (err && !user) { return done(err); }
       done(null, user);
