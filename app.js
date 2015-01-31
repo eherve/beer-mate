8000404a7a7a737ada665f407d393f4bbdbc6ee5
ROBERT François-Régis <robert.fregis@gmail.com>
2015-01-31 20:15:14 +0100

    remove express session redis

--- app.js
+++ app.js
@@ -22,14 +22,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Session
-var sessionConfig = require('./config/application.json').session;
-if (sessionConfig.redis) {
-  var RedisStore = require('connect-redis')(session);
-  app.use(session({
-    'store': new RedisStore(sessionConfig.redis),
-    'secret': sessionConfig.secret
-  }));
-} else { app.use(session(sessionConfig)); }
+// var sessionConfig = require('./config/application.json').session;
+// if (sessionConfig.redis) {
+//   var RedisStore = require('connect-redis')(session);
+//   app.use(session({
+//     'store': new RedisStore(sessionConfig.redis),
+//     'secret': sessionConfig.secret
+//   }));
+// } else { app.use(session(sessionConfig)); }
 
 app.use(passport.initialize());
 app.use(passport.session());
