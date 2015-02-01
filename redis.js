3e11374416ba06adc1fc0f47eddfebc1fdb18958
Eric Herve <eric.herve.95@gmail.com>
2015-02-01 22:31:17 +0100

    refactoring

--- redis.js
+++ redis.js
@@ -1,52 +1,53 @@
 'use strict';
 
 var util = require('util');
-var logger = require('./logger').get('redisAuth');
+var logger = require('./logger').get('Redis');
 var redis = require('redis');
-//redis.debug_mode = true;
 var redisCli = null;
 
 module.exports.connect = function(config, cb) {
-		var host = config.host;
-		var port = config.port;
-		var opts = config.options;
-		
-		redisCli = redis.createClient(port, host, opts);
-		redisCli.once('error', cb);
-		redisCli.once('ready', function() {
-				logger.debug('redis connected!');
-				this.removeListener('error', cb);
-				redisCli.on('error', function(err) {
-						console.error(util.format('%s:%s/%s %s', this.host, this.port, this.name, err));
-				});
-				cb();
-		});
+  var host = config.host;
+  var port = config.port;
+  var opts = config.options;
+  redisCli = redis.createClient(port, host, opts);
+  redisCli.once('error', cb);
+  redisCli.once('ready', function() {
+    logger.debug('redis connected!');
+    this.removeListener('error', cb);
+    redisCli.on('error', function(err) {
+      console.error(util.format('%s:%s/%s %s',
+        this.host, this.port, this.name, err));
+    });
+    cb();
+  });
 };
 
-module.exports.get = function(token, next) {
-		if (redisCli !== null && redisCli.connected) {
-				redisCli.get(token, next);
-		} else {
-				next(new Error('Redis not connected'));
-		}
+module.exports.get = function(token, cb) {
+  if (redisCli !== null && redisCli.connected) {
+    redisCli.get(token, cb);
+  } else {
+    cb(new Error('Reis server not connected'));
+  }
 };
 
-module.exports.register = function(token, userid, admin, next) {
-		if (redisCli !== null && redisCli.connected) {
-				if (token.trim() !== '' ) {
-						redisCli.set(token, JSON.stringify({id: userid, admin: admin, lastAccess: new Date()}), next);
-				} else {
-						next(new Error('didn\'t receive a token'));
-				}
-		} else {
-				next(new Error('Redis not connected'));
-		}
+module.exports.register = function(token, userid, admin, cb) {
+  if (redisCli !== null && redisCli.connected) {
+    if (token.trim() !== '' ) { // TODO remove, it will never happend
+      redisCli.set(token, JSON.stringify({
+        id: userid, admin: admin, lastAccess: Date.now()
+      }), cb);
+    } else {
+      cb(new Error('didn\'t receive a token'));
+    }
+  } else {
+    cb(new Error('Redis server not connected'));
+  }
 };
 
-module.exports.unregister = function(token, next) {
-		if (redisCli !== null && redisCli.connected) {
-				redisCli.del(token, next);
-		} else {
-				next(new Error('Redis not connected'));
-		}
+module.exports.unregister = function(token, cb) {
+  if (redisCli !== null && redisCli.connected) {
+    redisCli.del(token, cb);
+  } else {
+    cb(new Error('Redis server not connected'));
+  }
 };
