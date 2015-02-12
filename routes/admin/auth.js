a7ab28c0c62752f2fd9ac496c9cf71dc6dc12d86
Eric Herve <eherve@sedona.fr>
2015-02-12 14:22:32 +0100

    admin site restriction to administrator

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -7,6 +7,7 @@ var uuid = require('node-uuid');
 var logger = require('../../logger').get('Route');
 var NotFoundError = require('../../errors/notFoundError');
 var UnauthorizedError = require('../../errors/unauthorizedError');
+var ForbiddenError = require('../../errors/forbiddenError');
 var BadRequestError = require('../../errors/badRequestError');
 var ObjectId = require('mongoose').Types.ObjectId;
 var UserModel = require('../../models/user');
@@ -19,7 +20,7 @@ var CONFIRM_EXPIRATION_DELAY = 24 * 60 * 60 * 1000;
 // Login
 
 router.post('/login', function(req, res, next) {
-  Auth.userConnected(req, res, function(err) {
+  Auth.adminConnected(req, res, function(err) {
     if (!err) { return Auth.sendToken(res, req.redisData.token); }
     if (!req.body.email || !req.body.password) {
       return next(new BadRequestError());
@@ -29,6 +30,7 @@ router.post('/login', function(req, res, next) {
     UserModel.authenticate(email, password, function (err, user) {
       if (err && !user) { return next(err); }
       if (!user) { return next(new UnauthorizedError()); }
+      if (!user.administrator) { return next(new ForbiddenError()); }
       req.user = user;
       next();
     });
@@ -41,56 +43,5 @@ router.get('/logout', Auth.userConnected, function(req, res, next) {
   logger.debug(util.format('Logout user token: %s', req.redisData.token));
   next();
 }, Auth.logout);
-
-// Join
-
-function sendConfirmationEmail(req, user, token, expires) {
-  Resource.getEmailFile('confirmationEmail', req.locale, function(err, file) {
-    if (err) { return; }
-    Email.send(user.email, req.translate('email.confirmation'), file,
-    { host: req.get('host'), user: user, token: token, expires: expires,
-      creation: true }, function(err) {
-        if (err) { return; }
-        logger.debug('Validation email sent');
-      }
-    );
-  });
-}
-
-router.post('/join', function(req, res, next) {
-  var email = req.body.email;
-  var password = req.body.password;
-  if (!email || !password) { return next(new BadRequestError()); }
-  var token = uuid.v4();
-  var expires = new Date(Date.now() + CONFIRM_EXPIRATION_DELAY);
-  (new UserModel({ email: email, password: password,
-    validation: { token: token, expires: expires }
-  })).save(function(err, user) {
-    if (err) { return next(err); }
-    sendConfirmationEmail(req, user, token, expires);
-    res.end();
-  });
-});
-
-// Confirm
-
-router.get('/confirm/:userId/:token', function(req, res, next) {
-  var id = req.params.userId;
-  if (!ObjectId.isValid(id)) { return next(new NotFoundError()); }
-  UserModel.findById(id, function(err, user) {
-    if (err) { return next(err); }
-    if (!user) { return next(new NotFoundError()); }
-    if (user.validated === true) { return res.end(); } // TODO validate ok page ?
-    if (user.validation.token !== req.params.token ||
-      user.validation.expires < Date.now()) {
-        return next(new NotFoundError());
-    }
-    user.validated = true;
-    user.save(function(err) {
-      if (err) { return next(err); }
-      res.end(); // TODO validate ok page ?
-    });
-  });
-});
 
 module.exports = router;
