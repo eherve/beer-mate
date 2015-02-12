c3f44dfd00e5d8e2586ef6fc9d4bfa10c8e4d25c
Eric Herve <eherve@sedona.fr>
2015-02-12 11:20:53 +0100

    BadRequest error code fixed

--- errors/badRequestError.js
+++ errors/badRequestError.js
@@ -6,7 +6,7 @@ var BadRequestError = module.exports = function BadRequestError() {
   Error.call(this);
   this.message = 'Bad Request';
   this.stack = (new Error()).stack;
-  this.status = 404;
+  this.status = 400;
 };
 
 util.inherits(BadRequestError, Error);
