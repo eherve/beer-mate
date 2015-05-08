7f13cb52a5112fac33b931809bfc48ddba6c9de7
Eric Her <eric.herve.95@gmail.com>
2015-05-08 09:43:09 +0200

    cleanup

--- routes/admin/logger.js
+++ routes/admin/logger.js
@@ -2,15 +2,13 @@
 
 var express = require('express');
 var router = express.Router();
-var Auth = require('../../tools/auth');
 var logger = require('../../logger');
 
-router.get('/', Auth.adminConnected, function(req, res) {
-  res.locals.levels = logger.getLevels();
-  res.render('admin/logger');
+router.get('/', function(req, res, next) {
+  next(logger.getLevels());
 });
 
-router.post('/', Auth.adminConnected, function(req, res) {
+router.post('/', function(req, res) {
   logger.setLevels(req.body.logger, req.body.transport, req.body.level);
   res.end();
 });
