d242d5f8a27da16af64abd6ac8b1310bc3754789
Eric Her <eric.herve.95@gmail.com>
2015-05-09 11:09:06 +0200

    dmin user management preparation

---
+++ routes/admin/user.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+var logger = require('../../logger');
+
+router.get('/', function(req, res) {
+  
+});
+
+module.exports = router;
