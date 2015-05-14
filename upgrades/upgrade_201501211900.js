8e0c4d884dc2a7e0f8172e750d1b567eca04e7f7
Eric Her <eric.herve.95@gmail.com>
2015-05-14 22:15:50 +0200

    fix update

--- upgrades/upgrade_201501211900.js
+++ upgrades/upgrade_201501211900.js
@@ -3,7 +3,8 @@
 module.exports.upgrade = function(cb) {
   var UserModel = require('../models/user');
   var user = new UserModel({
-    email: 'admin@beermate.com', password: 'admin'
+    email: 'admin@beermate.com', password: 'admin',
+    firstname: 'admin', lastname: 'admin'
   });
   user.save(cb);
 };
